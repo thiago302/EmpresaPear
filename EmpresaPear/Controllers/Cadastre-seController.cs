fe72c56ffc26dcf78b1da09b542d0300258d0d31
thiago <thiagovr302@gmail.com>
2023-10-18 23:07:01 -0300

    adicionando uma validação de login.

---
+++ EmpresaPear/Controllers/Cadastre-seController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmpresaPear.Controllers
+{
+    public class Cadastre_seController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmpresaPear/Controllers/CadastreSeController.cs
@@ -0,0 +1,15 @@
+﻿using EmpresaPear.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpresaPear.Controllers
+{
+    public class CadastreSeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+     }
+}
+
